7e6e1537d5c84b9f22fe4771e19bbed009fe1f02
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-06-24 20:19:43 -0300

    feat: desafio 006

---
+++ Desafios/desafio06.py
@@ -0,0 +1,4 @@
+# Exercício 006
+import math
+num = int(input('Digite um número: '))
+print('Você digitou {}, o dobro é {}, o triplo é {} e a raiz quadrada é {}.'.format(num, num * 2, num * 3, math.sqrt(num)))
