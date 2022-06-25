63d1cb59171db31fecffedc292a4f9bba1965c6f
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-06-25 20:34:44 -0300

    feat: solução mais simples

--- Desafios/desafio017.py
+++ Desafios/desafio017.py
@@ -4,3 +4,7 @@ c_oposto = float(input('Comprimento do cateto oposto: '))
 c_adjacente = float(input('Comprimento do cateto adjacente: '))
 hipotenusa = math.sqrt(math.pow(c_oposto, 2) + math.pow(c_adjacente, 2))
 print('A hipotenusa vai medir {:.2f}'.format(hipotenusa))
+
+# proposta do professor mais simples
+hipotenusa = math.hypot(c_oposto, c_adjacente)
+print('A hipotenusa vai medir {:.2f}'.format(hipotenusa))
