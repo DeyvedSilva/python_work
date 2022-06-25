4e2bcb47617aaee09c0bf2dd8903c7e24fb87f4f
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-06-25 20:11:40 -0300

    feat: mudança na aula 08

--- Aulas/aula08.py
+++ Aulas/aula08.py
@@ -1,6 +1,5 @@
 import math
 import random
-# import emoji
 
 num = int(input('Digite um número: '))
 raiz = math.sqrt(num)
@@ -8,5 +7,3 @@ print('A raiz de {} é igual a {}'.format(num, math.ceil(raiz)))
 
 num = random.random()
 print(num)
-
-# print(emoji.emojize('Olá, mundo :earth_americas:', use_aliases = True))
