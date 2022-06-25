755f4fc730240bd0b229017a8061b6b52a32dce9
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-06-25 12:03:59 -0300

    feat: ajustes no desafio 06

--- Desafios/desafio06.py
+++ Desafios/desafio06.py
@@ -1,4 +1,7 @@
 # Exercício 006
 import math
 num = int(input('Digite um número: '))
-print('Você digitou {}, o dobro é {}, o triplo é {} e a raiz quadrada é {}.'.format(num, num * 2, num * 3, math.sqrt(num)))
+print('Você digitou {}, o dobro é {}, o triplo é {} e a raiz quadrada é {:.2f}.'.format(num, num * 2, num * 3, math.sqrt(num)))
+
+# outras possibilidades
+print('A raiz quadrada de {} é igual a {:.2f}.'.format(num, pow(num, 1/2)))
