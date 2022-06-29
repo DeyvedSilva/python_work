bd122f019a1387c61cef3115378e259a9939242e
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-06-29 12:17:07 -0300

    feat: solução aprimorada do desafio 24

--- Desafios/desafio024.py
+++ Desafios/desafio024.py
@@ -1,3 +1,3 @@
-cidade = str(input('Digite o nome de uma cidade: '))
-nome = cidade.split()
-print('O nome da cidade começa com "SANTO" {}'.format('SANTO' in nome[0]))
+cidade = str(input('Digite o nome de uma cidade: ')).strip()
+nome = cidade.lower().split()
+print('O nome da cidade começa com "SANTO" {}'.format('santo' in nome[0]))
