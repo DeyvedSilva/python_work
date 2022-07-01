1fdcbd15d3a9840cad643c8ee10a599e0ddd000f
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-01 20:05:29 -0300

    feat: melhoria na solução do desafio 39

--- Desafios/desafio039.py
+++ Desafios/desafio039.py
@@ -1,10 +1,14 @@
 from datetime import date
 ano = int(input('Informe seu ano de nascimento: '))
-ano_atual = date.today().year
-idade = int(ano_atual - ano)
-if idade < 18:
-    print('Ainda vai se alistar. Falta {} anos.'.format(18 - idade))
-elif idade == 18:
-    print('Está na hora de se alistar.')
+sexo = input('Qual o seu sexo? [H/M] ')
+if sexo == 'H':
+    ano_atual = date.today().year
+    idade = int(ano_atual - ano)
+    if idade < 18:
+        print('Ainda vai se alistar. Falta {} anos.'.format(18 - idade))
+    elif idade == 18:
+        print('Está na hora de se alistar.')
+    else:
+        print('Já passou do tempo do alistamento. Passou {} anos.'.format(idade - 18))
 else:
-    print('Já passou do tempo do alistamento. Passou {} anos.'.format(idade - 18))
+    print('Você é dispensada do alistamento militar.')
