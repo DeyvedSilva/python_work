38bac9caafc5c11d8f81bbc0350ff77310f6d25e
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-01 18:28:56 -0300

    feat: aula 12

---
+++ Aulas/aula12.py
@@ -0,0 +1,7 @@
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Deyved':
+    print('Que nome bonito!!')
+elif nome == 'Pedro' or nome == 'Maria':
+    print('Seu nome é popular.')
+else:
+    print('Seu nome é normal.')
