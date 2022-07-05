939b3e1f97362bee45470586bc016e29fa77879e
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-05 15:27:50 -0300

    feat: solução do desafio 61

---
+++ Desafios/desafio061.py
@@ -0,0 +1,7 @@
+termo = int(input('Informe o primeiro termo: '))
+razao = int(input('Informe a razão: '))
+cont = 0
+while cont < 10:
+    termo_novo = termo + cont * razao
+    cont += 1
+    print(termo_novo, end=' ')
