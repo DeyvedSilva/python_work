94e31daac3b38b8c5070782a310d6985453c03d3
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-07 17:27:00 -0300

    feat: melhoria proposta pelo professor do desafio 71

--- Desafios/desafio071.py
+++ Desafios/desafio071.py
@@ -2,19 +2,24 @@ print('~' * 30)
 print(' BANCO CEV '.center(30, '-'))
 print('~' * 30)
 valor = int(input('Que valor você quer sacar? R$'))
-ced_50 = valor // 50
-valor -= ced_50 * 50
-ced_20 = valor // 20
-valor -= ced_20 * 20
-ced_10 = valor // 10
-valor -= ced_10 * 10
-ced_1 = valor // 1
-if ced_50 != 0:
-    print(f'Total de {ced_50} cédulas de R$50')
-if ced_20 != 0:
-    print(f'Total de {ced_20} cédulas de R$20')
-if ced_10 != 0:
-    print(f'Total de {ced_10} cédulas de R$10')
-if ced_1 != 0:
-    print(f'Total de {ced_1} cédulas de R$1')
+total = valor
+ced = 50
+total_ced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        total_ced += 1
+    else:
+        if total_ced > 0:
+            print(f'Total de {total_ced} céulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        total_ced = 0
+        if total == 0:
+            break
 print('=' * 30)
+print('Obrigado')
