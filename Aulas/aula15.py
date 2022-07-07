f91ec1a97dd767d2a88754c46c17de2434b7251f
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-07 14:06:27 -0300

    test: aula 15

--- Aulas/aula15.py
+++ Aulas/aula15.py
@@ -1,5 +1,22 @@
-cont = 1
+# cont = 1
+# while True:
+#     print(cont, end='')
+#     cont += 1
+# print('Acabou')
+
+n = 0
+while n != 999:
+    n = int(input('Digite um número: '))
+
+n = s = 0
 while True:
-    print(cont, end='')
-    cont += 1
-print('Acabou')
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+print(f'A soma vale {s}')
+
+nome = 'José'
+idade = 33
+salario = 987.30
+print(f'O {nome:-^20} tem {idade} e ganha R${salario:.2f}.')
