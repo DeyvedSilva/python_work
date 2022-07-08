28b7ecaac3818063dad04307695c168b736aadf4
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-08 13:00:43 -0300

    feat: melhoria na solução do desafio 72

--- Desafios/desafio072.py
+++ Desafios/desafio072.py
@@ -2,6 +2,10 @@ tupla = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito
          'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 while True:
     num = int(input('Digite um número: [entre 0 e 20] '))
+    sair = ' '
     if 0 <= num <= 20:
         print(f'O número digitado foi {tupla[num]}')
+        while sair not in 'SN':
+            sair = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if sair == 'N':
         break
