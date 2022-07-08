f5efd867f170fb1010e9af1029c17821fcbf69eb
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-08 12:52:05 -0300

    feat: solução do desafio 72

---
+++ Desafios/desafio072.py
@@ -0,0 +1,7 @@
+tupla = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
+         'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    num = int(input('Digite um número: [entre 0 e 20] '))
+    if 0 <= num <= 20:
+        print(f'O número digitado foi {tupla[num]}')
+        break
