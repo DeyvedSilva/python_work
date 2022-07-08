8ef7afe4c64cece49ae41671e1dc2001803f636c
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-08 13:55:32 -0300

    feat: solução do desafio 74

---
+++ Desafios/desafio074.py
@@ -0,0 +1,5 @@
+from random import randint
+lista = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10),)
+print(f'Os valores sorteados foram {lista}')
+print(f'O maior valor é {max(lista)}')
+print(f'O menor valor é {min(lista)}')
