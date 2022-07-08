729965758538ee569d33026aa5c7f37cb692e58a
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-08 14:40:58 -0300

    feat: melhoria do professor na solução do desafio 75

--- Desafios/desafio075.py
+++ Desafios/desafio075.py
@@ -1,14 +1,10 @@
-num1 = int(input('Digite um número: '))
-num2 = int(input('Digite um número: '))
-num3 = int(input('Digite um número: '))
-num4 = int(input('Digite um número: '))
-tupla = (num1, num2, num3, num4)
-cont = 0
-for c in tupla:
-    if c == 9:
-        cont += 1
-print(f'O número 9 apareceu {cont} vezes')
-print(f'O número 3 está na posição {tupla.index(3) + 1}')
+tupla = (int(input('Digite um número: ')), int(input('Digite um número: ')), int(input('Digite um número: ')),
+         int(input('Digite um número: ')))
+print(f'O número 9 apareceu {tupla.count(9)} vezes')
+if 3 in tupla:
+    print(f'O número 3 está na posição {tupla.index(3) + 1}')
+else:
+    print('O valor 3 não aparece em nenhuma posição')
 print('Os pares são ', end='')
 for i in range(0, len(tupla)):
     if tupla[i] % 2 == 0:
