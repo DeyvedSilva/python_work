4f6a83a37c7250aa79a8a509ce361dd831c3dda7
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-10 12:27:53 -0300

    feat: solução do professor do desafio 83

--- Desafios/desafio083.py
+++ Desafios/desafio083.py
@@ -1,9 +1,16 @@
 expr = str(input('Digite a expressão: ')).strip()
 lista = []
 for c in expr:
-    lista.append(c)
-if lista.count('(') == lista.count(')') and lista.index('(') < lista.index(')'):
-    print('Sua expressão está correta!')
+    if c == '(':
+        lista.append('(')
+    elif c == ')':
+        if len(lista) > 0:
+            lista.pop()
+        else:
+            lista.append(')')
+            break
+if len(lista) == 0:
+    print('Sua expressão está válida!')
 else:
     print('Sua expressão está errada!')
-print(lista.index('(') < lista.index(')'))
+
