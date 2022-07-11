032418b2aae5cc508ba33ed4a7c8c8f1b5aad79a
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-11 06:12:45 -0300

    feat: solução do professor do desafio 86

--- Desafios/desafio086.py
+++ Desafios/desafio086.py
@@ -1,15 +1,10 @@
-lista = [[], [], []]
-a = b = 0
-for i in range(0, 9):
-    num = int(input(f'Digite um valor para [{a}, {b}]: '))
-    b += 1
-    lista[a].append(num)
-    if b == 3:
-        a += 1
-        b = 0
+lista = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for i in range(0, 3):
+    for j in range(0, 3):
+        lista[i][j] = int(input(f'Digite um valor para [{i}, {j}]: '))
 print('~-' * 30)
 for i in range(0, 3):
     for j in range(0, 3):
-        print(f'[{lista[i][j]:^3}]', end='')
+        print(f'[{lista[i][j]:^5}]', end='')
     print()
 
