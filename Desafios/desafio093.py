efdb2204c4cdd0f7c51a9ce2368fdfa0e238cdd4
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-12 13:35:07 -0300

    feat: melhoria da solução do desafio 93

--- Desafios/desafio093.py
+++ Desafios/desafio093.py
@@ -14,7 +14,7 @@ print('-=' * 30)
 for k, v in jogador.items():
     print(f'O campo {k} tem o valor {v}')
 print('-=' * 30)
-print(f'O jogador {jogador["Nome"]} jogou {partidas} partidas.')
+print(f'O jogador {jogador["Nome"]} jogou {len(jogador["gols"])} partidas.')
 for k, v in enumerate(gols):
     print(f'   => Na partida {k}, fez {v} gols.')
 print(f'Foi um total de {jogador["total"]} gols.')
