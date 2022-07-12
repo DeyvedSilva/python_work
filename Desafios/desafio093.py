df3fe0570cbe5234335cb5dd5165d27e80a698b7
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-12 13:31:21 -0300

    feat: solução do desafio 93

---
+++ Desafios/desafio093.py
@@ -0,0 +1,20 @@
+gols = []
+cont = 0
+jogador = {'Nome': str(input('Nome do jogador: '))}
+partidas = int(input(f'Quantas partidas {jogador["Nome"]} jogou? '))
+for i in range(0, partidas):
+    gol = int(input(f'Quantos gols na partida {i}? '))
+    gols.append(gol)
+    cont += gol
+print('-=' * 30)
+jogador['gols'] = gols[:]
+jogador['total'] = cont
+print(jogador)
+print('-=' * 30)
+for k, v in jogador.items():
+    print(f'O campo {k} tem o valor {v}')
+print('-=' * 30)
+print(f'O jogador {jogador["Nome"]} jogou {partidas} partidas.')
+for k, v in enumerate(gols):
+    print(f'   => Na partida {k}, fez {v} gols.')
+print(f'Foi um total de {jogador["total"]} gols.')
