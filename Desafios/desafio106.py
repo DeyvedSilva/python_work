e5428bcd4730b13f153a3813a0193b6ff9a901e2
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-13 18:02:06 -0300

    feat: solução do professor do desafio 106

--- Desafios/desafio106.py
+++ Desafios/desafio106.py
@@ -1,11 +1,37 @@
+from time import sleep
+c = ('\033[m',          # 0 - sem cors
+     '\033[0;30;41m',   # 1 - vermelho
+     '\033[0;30;42m',   # 2 - verde
+     '\033[0;30;43m',   # 3 - amarelo
+     '\033[0;30;44m',   # 4 - azul
+     '\033[0;30;45m',   # 5 - roxo
+     '\033[7;30m'       # 6 - branco
+     )
+
+
 def funcao(func):
-    tam = len(func) + 4 + len('Acessando o manual do comando ')
+    titulo(f'Acessando o manual do comando \'{func}\'', 4)
+    print(c[6], end='')
+    help(func)
+    print(c[0], end='')
+    sleep(2)
 
-    print('\033[44m~' * tam)
-    print(f'Acessando o manual do comando {func}'.center(tam))
+
+def titulo(txt, cor=0):
+    tam = len(txt) + 4
+    print(c[cor], end='')
     print('~' * tam)
-    print(f'\033[7:40m{help(func)}')
-    print()
+    print(f'  {txt}')
+    print('~' * tam)
+    print(c[0], end='')
+    sleep(1)
 
 
-funcao(str(input('Função ou Biblioteca > ')))
+while True:
+    titulo('SISTEMA DE AJUDA PyHELP', 2)
+    comando = str(input('Função ou Biblioteca > '))
+    if comando.upper() == 'FIM':
+        break
+    else:
+        funcao(comando)
+titulo('ATÉ LOGO', 1)
