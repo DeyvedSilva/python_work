cfa8ea59015948dc4c2c2d26fe503565dcd9bd15
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-14 09:53:09 -0300

    feat: solução do desafio 107

---
+++ Modulos/moeda.py
@@ -0,0 +1,19 @@
+def aumentar(valor, taxa):
+    novo = valor + (valor * taxa / 100)
+    return novo
+
+
+def diminuir(valor, taxa):
+    novo = valor - (valor * taxa / 100)
+    return novo
+
+
+def dobro(valor):
+    novo = valor * 2
+    return novo
+
+
+def metade(valor):
+    novo = valor / 2
+    return novo
+
