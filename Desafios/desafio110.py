f06505e5f71be928b3c0494d287353b3c89b45e7
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-14 11:11:47 -0300

    feat: solução do desafio 110

---
+++ Desafios/desafio110.py
@@ -0,0 +1,4 @@
+from Modulos import moeda
+
+p = float(input('Digite o preço: R$'))
+moeda.resumo(p, 80, 35)
--- Modulos/moeda.py
+++ Modulos/moeda.py
@@ -27,3 +27,14 @@ def metade(valor, formatacao=False):
 def moeda(valor):
     formato = f'R${valor:.2f}'.replace('.', ',')
     return formato
+
+
+def resumo(preco, taxaa, taxar):
+    print('-' * 30)
+    print(f'{"RESUMO DO VALOR":^30}')
+    print('-' * 30)
+    print(f'{"Preço analisado:":<20}{moeda(preco)}')
+    print(f'{"Dobro do preço:":<20}{dobro(preco, True)}')
+    print(f'{"Metade do preço:":<20}{metade(preco, True)}')
+    print(f'{"80% de aumento:":<20}{aumentar(preco, taxaa, True)}')
+    print(f'{"35% de redução:":<20}{diminuir(preco, taxar, True)}')
