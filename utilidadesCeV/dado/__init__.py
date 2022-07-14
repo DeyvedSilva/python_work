c71f2564d2342f7b20c4470cadfb141a326e99ea
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-14 12:44:26 -0300

    feat: melhoria na solução do desafio 112

--- utilidadesCeV/dado/__init__.py
+++ utilidadesCeV/dado/__init__.py
@@ -1,7 +1,7 @@
 def leia_dinheiro(txt):
     while True:
-        msg = input(txt)
-        if msg.isnumeric():
-            return int(msg)
+        msg = str(input(txt)).replace(',', '.').strip()
+        if msg.isalpha() or msg != '':
+            return float(msg)
         else:
             print(f'\033[31mERRO! \"{msg}\" é um preço inválido\033[m')
