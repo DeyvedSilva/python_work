11f9a8e59839dffbdfccc402cd4c7c89ac3789ec
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-14 10:54:47 -0300

    feat: melhoria na solução do desafio 109

--- Modulos/moeda.py
+++ Modulos/moeda.py
@@ -2,28 +2,26 @@ def aumentar(valor, taxa, formatacao=False):
     novo = valor + (valor * taxa / 100)
     if formatacao:
         return moeda(novo)
-    return novo
+    else:
+        return novo
 
 
 def diminuir(valor, taxa, formatacao=False):
     novo = valor - (valor * taxa / 100)
-    if formatacao:
-        return moeda(novo)
-    return novo
+    return novo if not formatacao else moeda(novo)
 
 
 def dobro(valor, formatacao=False):
     novo = valor * 2
-    if formatacao:
-        return moeda(novo)
-    return novo
+    return novo if not formatacao else moeda(novo)
 
 
 def metade(valor, formatacao=False):
     novo = valor / 2
     if formatacao:
         return moeda(novo)
-    return novo
+    else:
+        return novo
 
 
 def moeda(valor):
