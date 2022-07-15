0571b51a5210fb42e152e9e4fd63ce797d125169
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-15 17:21:26 -0300

    feat: solução do desafio 115 A

---
+++ Desafios/desafio115.py
@@ -0,0 +1,4 @@
+import Modulos
+
+Modulos.criar_menu()
+opcao = str(input('\033[33mSua opção:\033[m '))
---
+++ Modulos/__init__.py
@@ -0,0 +1,8 @@
+def criar_menu():
+    print('-' * 60)
+    print('MENU PRINCIPAL'.center(60))
+    print('-' * 60)
+    print("""1 - Ver pessoas cadastradas
+2 - Cadastrar nova pessoa
+3 - Sair do sistema""")
+    print('-' * 60)
