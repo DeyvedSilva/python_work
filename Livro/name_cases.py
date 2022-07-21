1f21d71551954a522426db5d6c309d3d441b9502
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-20 23:14:51 -0300

    feat: cap. 2 do livro referência

---
+++ Livro/name_cases.py
@@ -0,0 +1,27 @@
+pessoal = 'Davi'
+print('Alô ' + pessoal + ', você gostaria de aprender um pouco de Python hoje?')
+
+print(pessoal.lower())
+print(pessoal.upper())
+print(pessoal.title())
+
+print('Albert Einstein certa vez disse: "Uma pessoa que nunca cometeu um erro jamais tentou nada novo."')
+
+famous_person = 'Albert Einstein'
+message = '"Uma pessoa que nunca cometeu um erro jamais tentou nada novo."'
+print(famous_person + ' certa vez disse: ' + message)
+
+pessoa = '   Deyved    '
+print(pessoa.strip())
+print(pessoa.lstrip())
+print(pessoa.rsplit())
+
+print(5 + 3)
+print(9 - 1)
+print(4 * 2)
+print(16 // 2)
+
+favorito = 16
+mensagem = 'Meu número favorito é ' + str(favorito)
+print(mensagem)
+
