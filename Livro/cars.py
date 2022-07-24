1bd004119d74e299bfb32975d28a2bdc9d4cc90d
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-24 09:19:17 -0300

    feat: funções sort e sorted

---
+++ Livro/cars.py
@@ -0,0 +1,10 @@
+cars = ['bmw', 'audi', 'toyota', 'saburu']
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'saburu']
+print(f'Here is the original list: {cars}')
+print(f'Here is the sorted list: {sorted(cars)}')
+print(f'Here is the original list again: {cars}')
