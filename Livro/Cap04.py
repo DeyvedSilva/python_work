d47b4b6c43bd871dee3ac9dc1f15390768ee2be6
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-25 23:46:41 -0300

    feat: exercícios do cap. 4

---
+++ Livro/Cap04.py
@@ -0,0 +1,13 @@
+# exercícios do capítulo 4
+# 4.1
+pizzas_favoritas = ['Franbacon', 'Frango com catupyri', 'Portuguesa']
+for pizza in pizzas_favoritas:
+    print(f'Gosto de pizza de {pizza}.')
+
+print('Eu realmente adoro pizza!\n')
+
+# 4.2
+animais = ['belo', 'cooke', 'negão']
+for animal in animais:
+    print(f'O {animal.title()} é um ótimo animal de estimação.')
+print('Qualquer um desses animais é um ótimo animal de estimação!\n')
