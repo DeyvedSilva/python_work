8cead65bbe8f0994038cf59fee15a59df41a5b40
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-07-25 23:46:22 -0300

    feat: prática da teoria do cap. 4

---
+++ Livro/numbers.py
@@ -0,0 +1,10 @@
+for value in range(1, 5):
+    print(value)
+
+numbers = list(range(1, 6))
+print(numbers)
+
+squares = []
+for squar in range(1, 11):
+    squares.append(squar ** 2)
+print(squares)
