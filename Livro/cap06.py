ea4119c254c4430c7781d079966f8a3868c64688
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-08-31 14:10:27 -0300

    feat: exercícios do cap. 6

--- Livro/cap06.py
+++ Livro/cap06.py
@@ -15,3 +15,30 @@ glossario = {'and': 'operador lógico', 'or': 'operador lógico', 'not': 'operad
              'modulos': 'funções de classes', }
 for chave, valor in glossario.items():
     print(f'{chave}: \n\t{valor}')
+
+# 6.4
+glossario['in'] = 'Está na lista'
+glossario['class'] = 'Classe, molde para criar objetos'
+glossario['if'] = 'instrução condicional'
+glossario['for'] = 'laço de repetição'
+glossario['while'] = 'laço de repetição'
+for chave, valor in glossario.items():
+    print(f'{chave}: {valor}')
+
+# 6.5
+rios = {'nilo': 'egito', 'amazonas': 'brasil', 'ganges': 'índia'}
+for chave, valor in rios.items():
+    print(f'O {chave.title()} corre pelo(a) {valor.title()}.')
+for name in rios:
+    print(name.title())
+for pais in rios.values():
+    print(pais.title())
+
+# 6.6
+favorite_languages = {'jen': 'python', 'sarah': 'c', 'edward': 'ruby', 'phil': 'python'}
+pessoas = ['edward', 'maria', 'josé', 'phil']
+for pessoa in sorted(pessoas):
+    if pessoa in favorite_languages:
+        print(f'{pessoa}, obrigado por participar da enquete.')
+    else:
+        print(f'{pessoa}, vamos participar da enquete!')
