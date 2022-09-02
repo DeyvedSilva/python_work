3a570ff6826cf9a965a4cd1ad1b0b7e30eff11c1
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-09-02 10:49:09 -0300

    feat: exercícios do cap. 6

--- Livro/cap06.py
+++ Livro/cap06.py
@@ -1,8 +1,8 @@
 # exercícios do capítulo 6
 """"""
 # 6.1
-pessoa = {'first name': 'Deyved', 'last name': 'Silva', 'age': 36, 'city': 'Cariacica'}
-for chave, valor in pessoa.items():
+pessoa_0 = {'first name': 'Deyved', 'last name': 'Silva', 'age': 36, 'city': 'Cariacica'}
+for chave, valor in pessoa_0.items():
     print(f'{chave}: {valor}')
 
 # 6.2
@@ -42,3 +42,42 @@ for pessoa in sorted(pessoas):
         print(f'{pessoa}, obrigado por participar da enquete.')
     else:
         print(f'{pessoa}, vamos participar da enquete!')
+
+# 6.7
+pessoa_1 = {'first name': 'Jane', 'last name': 'Silva', 'age': 46, 'city': 'Cariacica'}
+pessoa_2 = {'first name': 'Davi', 'last name': 'Silva', 'age': 4, 'city': 'Cariacica'}
+people = [pessoa_0, pessoa_1, pessoa_2]
+for gente in people:
+    print(gente)
+
+# 6.8
+pet_0 = {'tipo': 'cão', 'dono': 'João', }
+pet_1 = {'tipo': 'gato', 'dono': 'João', }
+pet_2 = {'tipo': 'cão', 'dono': 'Eduardo', }
+pet_3 = {'tipo': 'gato', 'dono': 'Maria', }
+pets = [pet_0, pet_1, pet_2, pet_3]
+for pet in pets:
+    print(pet)
+
+# 6.9
+favorite_places = {'Deyved': ['PE', 'CE'], 'Jane': ['RJ', 'BA', 'ES'], 'Giulia': ['RJ'], }
+for pessoa, lugar in favorite_places.items():
+    print(f'nome: {pessoa}, lugares favoritos: {lugar}')
+
+# 6.10
+numeros_favoritos = {'Jane': [46], 'Deyved': [36, 16, 64], 'Giulia': [18], 'Pedro': [6, 10], 'Davi': [4, 5, 2], }
+for nome, valor in numeros_favoritos.items():
+    print(f'{nome}:\t {valor}')
+
+# 6.11
+cities = {'Rio de Janeiro': {'country': 'Brasil', 'population': 150000, 'fact': 'sudeste'},
+          'Recife': {'country': 'Brasil', 'population': 145000, 'fact': 'nordeste'},
+          'Vitória': {'country': 'Brasil', 'population': 120000, 'fact': 'sudeste'}, }
+for city, city_inf in cities.items():
+    print(f'\nCidade: {city}')
+    pais = city_inf['country']
+    populacao = city_inf['population']
+    fato = city_inf['fact']
+    print(f'País: \t{pais}')
+    print(f'População: \t{populacao}')
+    print(f'Fato: \t{fato}')
