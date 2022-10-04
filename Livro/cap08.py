7e37fae739e81eb34b1ee068210181b78883ef56
DeyvedSilva <deyved.silva@blueshift.com.br>
2022-10-04 11:40:56 -0300

    feat: exercícios do cap. 8

---
+++ Livro/cap08.py
@@ -0,0 +1,15 @@
+# exercícios do capítulo 8
+# 8.1
+def display_message():
+    print('Estou aprendendo sobre função.')
+
+
+display_message()
+
+
+# 8.2
+def favorite_book(title):
+    print(f'Um dos meus livros favoritos é {title}')
+
+
+favorite_book('O poder do hábito')
